3a9ab53d157a2513ee02331296b3a2f609a7b5b6
Ruben Santos <1130523@isep.ipp.pt>
2015-12-15 17:59:21 +0000

    Created method to export information html and interface Export

---
+++ src/Legacy/ExportHTML.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Legacy;
+
+/**
+ *
+ * @author ruben
+ */
+public class ExportHTML implements Export {
+
+	public String export(String text) {
+		String aux = this.begin();
+		aux += this.addText(text);
+		aux += this.endFile();
+		return aux;
+	}
+
+	private static String begin() {
+		return "<!DOCTYPE> <html>   <head>       <title>Projeto de FSIAP</title>       <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">       <link rel=\"icon\" type=\"image/ico\" href=\"http://www.isep.ipp.pt/favicon.ico\">   </head>   <body>       <div align=\"center\">       <img src=\"http://www.dei.isep.ipp.pt/images/topo_index.png\" alt=\"Logotipo ISEP\" >       </div>";
+	}
+
+	private String addText(String text) {
+		String aux = " <div align=\"center\">  " + text + "   </div>";
+
+		return aux;
+	}
+
+	private String endFile() {
+		return "</body></html>";
+	}
+
+}
