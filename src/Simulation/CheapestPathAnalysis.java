5dedf3bba4cdbddcd1bfb51d09e6b07c6f981251
1141330 <1141330@isep.ipp.pt>
2015-12-18 15:52:55 +0000

    CheapestPathAnalysis Interaction

--- src/Simulation/CheapestPathAnalysis.java
+++ src/Simulation/CheapestPathAnalysis.java
@@ -7,8 +7,13 @@ package Simulation;
 
 import Model.Node;
 import Model.RoadNetwork;
+import Model.Section;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -22,11 +27,25 @@ public class CheapestPathAnalysis implements PathAnalysis {
 
 	public List<String[]> analyze(RoadNetwork roadNetwork, Node startNode,
 								  Node endNode) {
-		List<String[]> results = new ArrayList();
-		results.add("Cheapest;Path;Analysis".split(";"));
-		results.add("1;2;3".split(";"));
-		results.add("4;5;6".split(";"));
-		return results;
+		Map<Double, List<String[]>> results = new HashMap();
+		List<Deque<Section>> sectionPaths = new ArrayList();
+		List<Deque<Node>> nodesPaths = roadNetwork.
+			getAllPaths(startNode, endNode, sectionPaths);
+		Double total = 0.0;
+		for (Deque<Section> sectionPath : sectionPaths) {
+			List<String[]> result = new ArrayList();
+			for (Section section : sectionPath) {
+				StringBuilder stringBuilder = new StringBuilder();
+				total = total + section.getToll();
+				stringBuilder.append("Section:;").append(section.getRoad()).
+					append("Section Toll Cost:;").append(section.getToll()).
+					append("Section Total Toll Cost:").append(total);
+				result.add(stringBuilder.toString().split(";"));
+			}
+			results.put(total, result);
+		}
+		return results.get(Collections.min(results.keySet()));
+
 	}
 
 }
