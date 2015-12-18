107b05adfc4f045ea73d50f07d1f479d23165912
Ruben Santos <1130523@isep.ipp.pt>
2015-12-18 16:02:02 +0000

    Level path analysis created

--- src/Simulation/LevelPathAnalysis.java
+++ src/Simulation/LevelPathAnalysis.java
@@ -7,8 +7,14 @@ package Simulation;
 
 import Model.Node;
 import Model.RoadNetwork;
+import Model.Section;
+import Model.Segment;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -22,11 +28,31 @@ public class LevelPathAnalysis implements PathAnalysis {
 
 	public List<String[]> analyze(RoadNetwork roadNetwork, Node startNode,
 								  Node endNode) {
-		List<String[]> results = new ArrayList();
-		results.add("Level;Path;Analysis".split(";"));
-		results.add("1;2;3".split(";"));
-		results.add("4;5;6".split(";"));
-		return results;
+		Map<Double, List<String[]>> results = new HashMap();
+		List<Deque<Section>> sectionPaths = new ArrayList();
+		List<Deque<Node>> nodesPaths = roadNetwork.
+			getAllPaths(startNode, endNode, sectionPaths);
+		for (Deque<Section> sectionPath : sectionPaths) {
+			Double total = 0.0;
+			List<String[]> result = new ArrayList();
+			result.add("Segment;Slope of Segment;Total Slope;".split(";"));
+			for (Section section : sectionPath) {
+				for (Segment segment : section.getSegments()) {
+					total += (segment.getSlope());
+					StringBuilder stringBuilder = new StringBuilder();
+
+					stringBuilder.append(segment.toString());
+
+					stringBuilder.append(segment.getSlope());
+
+					stringBuilder.append(total);
+					result.add(stringBuilder.toString().split(";"));
+
+				}
+			}
+			results.put(total, result);
+		}
+		return results.get(Collections.min(results.keySet()));
 	}
 
 }
