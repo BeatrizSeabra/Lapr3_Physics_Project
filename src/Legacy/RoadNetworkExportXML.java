e359896c9cf0e2fd1202fa3b24183da95b03916b
1131190 <1131190@isep.ipp.pt>
2016-01-10 01:21:45 +0000

    Update in the tests od exportProjectXml with a new file. Corretions in general

--- src/Legacy/RoadNetworkExportXML.java
+++ src/Legacy/RoadNetworkExportXML.java
@@ -53,78 +53,78 @@ public class RoadNetworkExportXML implements Export {
 				stringBuilder.append(node.getName());
 				stringBuilder.append("\"/>\n");
 			}
-			stringBuilder.append("	</node_list>");
-			stringBuilder.append("	<section_list>");
+			stringBuilder.append("\t</node_list>");
+			stringBuilder.append("\t<section_list>");
 			for (Section section : roadNetwork.getSections()) {
 				Deque<Node> nodes = roadNetwork.getExtremeNodes(section);
-				stringBuilder.append("		<road_section begin=\"");
+				stringBuilder.append("\t\t<road_section begin=\"");
 				stringBuilder.append(nodes.getFirst().getName());
 				stringBuilder.append("\" end=\"");
 				stringBuilder.append(nodes.getLast().getName());
 				stringBuilder.append("\">\n");
-				stringBuilder.append("			<road>");
+				stringBuilder.append("\t\t\t<road>");
 				stringBuilder.append(section.getRoad());
 				stringBuilder.append("</road>\n");
-				stringBuilder.append("			<typology>");
+				stringBuilder.append("\t\t\t<typology>");
 				stringBuilder.append(section.getTypology());
 				stringBuilder.append("</typology>\n");
-				stringBuilder.append("			<direction>");
+				stringBuilder.append("\t\t\t<direction>");
 				stringBuilder.append(section.getDirection());
 				stringBuilder.append("</direction>\n");
-				stringBuilder.append("			<toll>");
+				stringBuilder.append("\t\t\t<toll>");
 				stringBuilder.append(section.getToll().getValue());
 				stringBuilder.append(" ");
 				stringBuilder.append(section.getToll().getUnit());
 				stringBuilder.append("</toll>\n");
-				stringBuilder.append("			<wind_direction>");
+				stringBuilder.append("\t\t\t<wind_direction>");
 				stringBuilder.append(section.getWindDirection().getValue()).
 					append(" ");
 				stringBuilder.append(section.getWindDirection().getUnit());
 				stringBuilder.append("</wind_direction>\n");
-				stringBuilder.append("			<wind_speed>");
+				stringBuilder.append("\t\t\t<wind_speed>");
 				stringBuilder.append(section.getWindSpeed().getValue()).
 					append(" ");
 				stringBuilder.append(section.getWindSpeed().getUnit());
 				stringBuilder.append("</wind_speed>\n");
-				stringBuilder.append("			<segment_list>");
+				stringBuilder.append("\t\t\t<segment_list>");
 				for (Segment segment : section.getSegments()) {
-					stringBuilder.append("			<segment id=\"");
+					stringBuilder.append("\t\t\t\t<segment id=\"");
 					stringBuilder.append(segment.getName());
 					stringBuilder.append("\">\n");
-					stringBuilder.append("			<height>");
+					stringBuilder.append("\t\t\t\t\t<height>");
 					stringBuilder.append(segment.getHeight().getValue()).
 						append(" ");
 					stringBuilder.append(segment.getHeight().getUnit());
 					stringBuilder.append("</height>\n");
-					stringBuilder.append("			<slope>");
+					stringBuilder.append("\t\t\t\t\t<slope>");
 					stringBuilder.append(segment.getSlope().getValue()).
 						append(" ");
 					stringBuilder.append(segment.getSlope().getUnit());
 					stringBuilder.append("</slope>\n");
-					stringBuilder.append("			<length>");
+					stringBuilder.append("\t\t\t\t\t<length>");
 					stringBuilder.append(segment.getLength().getValue()).
 						append(" ");
 					stringBuilder.append(segment.getLength().getUnit());
 					stringBuilder.append("</length>\n");
-					stringBuilder.append("			<max_velocity>");
+					stringBuilder.append("\t\t\t\t\t<max_velocity>");
 					stringBuilder.append(segment.getMaxVelocity().getValue()).
 						append(" ");
 					stringBuilder.append(segment.getMaxVelocity().getUnit());
 					stringBuilder.append("</max_velocity>\n");
-					stringBuilder.append("			<min_velocity>");
+					stringBuilder.append("\t\t\t\t\t<min_velocity>");
 					stringBuilder.append(segment.getMinVelocity().getValue()).
 						append(" ");
 					stringBuilder.append(segment.getMinVelocity().getUnit());
 					stringBuilder.append("</min_velocity>\n");
-					stringBuilder.append("			<number_vehicles>");
+					stringBuilder.append("\t\t\t\t\t<number_vehicles>");
 					stringBuilder.append(segment.getNumberVehicles());
 					stringBuilder.append("</number_vehicles>\n");
-					stringBuilder.append("				</segment>\n");
+					stringBuilder.append("\t\t\t\t</segment>\n");
 				}
-				stringBuilder.append("			</segment_list>\n");
+				stringBuilder.append("\t\t\t</segment_list>\n");
 			}
-			stringBuilder.append("		</road_section>\n");
-			stringBuilder.append("	</section_list>\n");
+			stringBuilder.append("\t\t</road_section>\n");
+			stringBuilder.append("\t</section_list>\n");
 			stringBuilder.append("</Network>");
 			return stringBuilder.toString();
 		}
